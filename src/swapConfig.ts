97d49e0a83c4e99be07dd0c4e56152ffd91721a1
shrkz1 <shrkz1@gmail.com>
2024-06-04 06:28:21 +0200

    commented token amount in swapConfig.ts

--- src/swapConfig.ts
+++ src/swapConfig.ts
@@ -1,7 +1,7 @@
 export const swapConfig = {
   executeSwap: true, // Send tx when true, simulate tx when false
   useVersionedTransaction: true,
-  tokenAAmount: 0.1, // Swap 0.01 SOL for USDT in this example
+  // tokenAAmount: 0.1, // Swap 0.01 SOL for USDT in this example
   // tokenAAddress: "So11111111111111111111111111111111111111112", // Token to swap for the other, SOL in this case
   // tokenBAddress: "EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v",
   maxLamports: 1500000, // Micro lamports for priority fee
